61e26d48289c9eb30f323ec670b4747b9b99494f
Serious-Fin <augustasgri@gmail.com>
2023-05-17 23:42:52 +0300

    Succesfully add User model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
+class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum: 3}, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: true
+  validates :password, presence: true, length: {minimum: 5}
+end
